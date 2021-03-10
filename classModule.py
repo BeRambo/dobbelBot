8a8c87eeaa208066da52a41282659fd3cc127b26
BeRambo <bramvlassaks@hotmail.com>
2021-03-10 11:34:59 +0100

    Initial files
    need to add images

---
+++ classModule.py
@@ -0,0 +1,192 @@
+import random
+import copy
+
+class Player:
+  def __init__(self, author):
+    self.name = author.name
+    self.discriminator = author.id
+    self.score = 0
+    self.tries = 0
+    self.drawScore = 0
+    self.active = False
+
+  def setScore(self, Score):
+    self.score = Score.score
+    self.tries = Score.tries
+
+
+class Score:
+    def __init__(self, name='' , discriminator='', score = 0, tries = 3):
+        self.name = name
+        self.discriminator = discriminator
+        self.score = score
+        self.tries = tries
+    
+    def setScore(self, Player):
+        self.name = Player.name
+        self.discriminator = Player.discriminator
+        self.score = Player.score
+        self.tries = Player.tries
+
+    def resetScore(self):
+      self = self.__init__()
+
+    def printScore(self):
+      if self.score in range(294, 301):
+        if self.score == 300:
+          repl = '3 apen, kut'
+        elif self.score == 299:
+          repl = 'Soixant neuf'
+        else:
+          zandScore = 300 - self.score
+          repl = 'Zand van ' + str(zandScore)
+      elif self.score == 7: repl = '7, adje kut!'
+      else: repl = self.score
+      return str(repl)
+      
+
+class Game:
+  def __init__(self):
+    self.highScore = Score()
+    self.participants = []
+    self.started = False
+    self.roundsPlayed = 0
+    self.drawPlayers = []
+  
+  def addPlayer(self, Player):
+    self.participants.append(Player)
+
+  def showPlayers(self):
+    for p in self.participants: print(f"{p.name}, {p.discriminator}\n")
+
+  def start(self):
+    self.started = True
+    randomInt = random.randint(0, len(self.participants)-1)
+    self.rotatePlayers(randomInt) #chooses a random startplayer, and puts it first in the list
+
+  def stop(self):
+    self.started = False
+
+  def retrievePosition(self, idToFind):
+    for i, p in enumerate(self.participants): #finds the position in participants, based on id
+      if p.discriminator == idToFind:
+        return i
+
+  def getPlayer(self, id):
+    playerPos = self.retrievePosition(id) #get the position of the player in the participants array
+    return self.participants[playerPos]
+
+  def getActivePlayer(self, drawGame = False): #returns the active player, std from participants. if true, taken from drawPlayers
+    if drawGame:
+      li = self.drawPlayers
+    else:
+      li = self.participants
+
+    for player in li:
+      if player.active:
+        return player
+
+  def checkActive(self, id):
+    playerObject = self.getPlayer(id)
+    if playerObject.active:
+      return True
+    else: return False
+
+  def rotatePlayers(self, n):
+    for i in range(0, n):
+      self.participants.append(self.participants.pop(0))
+
+  def newRound(self):
+    for p in self.participants:
+      p.score = 0
+      p.tries = 0
+    self.roundsPlayed += 1
+    self.highScore.resetScore()
+    self.drawPlayers = []
+
+  def setHighScore(self, Player):
+    if self.highScore.name != '': #keeps the tries if the score is improved
+      Player.tries = copy.copy(self.highScore.tries)
+    self.highScore.setScore(Player)
+
+  def checkDraw(self): #checks if someone has the same score as the highscore
+    for player in self.participants:
+      if player.score == self.highScore.score and player.name != self.highScore.name:
+        self.drawPlayers.append(player)
+
+    if self.drawPlayers: #adds original winner to drawPlayers
+      posOrigWinner = self.retrievePosition(self.highScore.discriminator)
+      self.drawPlayers.append(self.participants[posOrigWinner]) 
+
+  def checkWinner(self):
+    if not self.drawPlayers: #if no drawgame, winner is in highScore
+      id = self.highScore.discriminator
+    else:
+      id = self.drawPlayers[0].discriminator
+
+    winnerPos = self.retrievePosition(id)
+    self.rotatePlayers(winnerPos)
+
+  def removeDrawPlayers(self): #removes all players with a lower draw score then the highest
+    highestThrow = self.drawPlayers[0].drawScore
+    for player in self.drawPlayers:
+      if player.drawScore < highestThrow:
+        self.drawPlayers.remove(player)
+
+
+class Dice:
+  def __init__(self, sides = 6):
+    self.sides = sides
+
+  def roll(self):
+    return random.randint(1, self.sides)
+
+
+class Throw:
+  '''
+  stores the throw in an array, diceThrow
+  countThrow counts the throw, stored in self.score
+  printThrow prints the throw, returns it directly
+  '''
+  def __init__(self):
+    self.score = 0
+    self.throwArray = []
+    self.shake()
+    self.countThrow()
+
+  def shake(self):
+    self.throwArray = [Dice().roll(), Dice().roll(), Dice().roll()]
+    self.countThrow()
+
+  def countThrow(self):
+    self.score = 0
+    throw = self.throwArray
+    if throw[0] == throw[1] and throw[1] == throw[2]: #checkt voor zand
+      if throw[0] == 1:
+        self.score = 300 #zand van 1 = 300
+      else:
+        self.score = 300 - throw[0] #2 = 298, 3 = 297, 4=296, 5=295, 6=294
+                
+    else:
+      for dice in throw:
+        if dice == 1:
+          self.score += 100
+
+        elif dice == 6:
+          self.score += 60
+
+        else: self.score += dice
+
+        if self.score == 69: self.score = 299
+
+  def printThrow(self):
+    if self.score in range(294, 301):
+      if self.score == 300:
+        return '3 apen, kut'
+      elif self.score == 299:
+        return 'Soixant neuf'
+      else:
+        zandScore = 300 - self.score
+        return 'Zand van ' + str(zandScore)
+    elif self.score == 7: return '7, adje kut!'
+    else: return self.score 
---
+++ main.py
@@ -0,0 +1,155 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+from classModule import Game
+from classModule import Player
+from classModule import Score
+from classModule import Dice
+from classModule import Throw
+
+#variables for the discord bot
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix= "$", intents=intents)
+
+#variables for the game
+game = Game()
+worp = Throw()
+
+#other functions
+def takeDrawScore(Player):
+  return Player.drawScore
+
+@bot.event
+async def on_ready():
+    print('We have logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-----')
+    await bot.change_presence(activity=discord.Game(name='1-2-en'))
+
+@bot.command()
+async def join(ctx):
+  if not game.started:
+    player = Player(ctx.author)
+    game.addPlayer(player)
+    await ctx.send("{} is toegevoegd aan de lijst met spelers!".format(player.name))
+    game.showPlayers() #debug option
+  else:
+    await ctx.send("Er is nog een spel bezig, je kan pas meedoen bij een nieuw spel.")
+
+
+@bot.command()
+async def gooi(ctx):
+  if game.started:
+    if game.checkActive(ctx.author.id): #checks if this is the active player
+      worp.shake()
+      for dobbelsteen in worp.throwArray: #stuur foto van dobbelsteen
+        image = discord.File('img/' + str(dobbelsteen) + '.png')
+        await ctx.send(file=image)
+
+      await ctx.send(worp.printThrow())
+
+
+@bot.command()
+async def start(ctx):
+  #start phase
+  if len(game.participants) == 1:
+    await ctx.send('Ge kunt geen spel alleen spelen, zuiplap!')
+  elif len(game.participants) == 0:
+    await ctx.send('Typ $join om mee te doen aan het spel. Wanneer iedereen klaar is, typ je $start om het spel te starten.')
+  else:
+    game.start()
+    await ctx.send(f"{game.participants[0].name} mag beginnen!")
+
+  #functions for input validation
+  def checkGooi(m):
+    return m.content == "$gooi" and m.channel == ctx.channel and m.author.id == game.getActivePlayer().discriminator
+  def checkGooiEen(m):
+    return m.content == "$gooiEen" and m.channel == ctx.channel and m.author.id == game.getActivePlayer(True).discriminator
+  def checkAnswer(m):
+    msgContent = m.content[0].lower()
+    return msgContent == "j" and m.author.id == game.getActivePlayer().discriminator or msgContent == "n" and m.channel == ctx.channel and m.author.id == game.getActivePlayer().discriminator
+
+
+  while game.started and len(game.participants) >= 1:
+    game.newRound()
+
+    for player in game.participants: #elke speler 1x aan de beurt
+      player.active = True #starts turn
+      mention = '<@'+str(player.discriminator)+'>'
+      await ctx.send(mention + " is aan de beurt!")
+
+      if player != game.participants[0]: #geeft highscore weer bij alle spelers, behalve de eerste
+        await ctx.send(f"De hoogste score is {game.highScore.printScore()}, door {game.highScore.name} in {game.highScore.tries} tries")
+
+      for i in range(1, game.highScore.tries+1): #max zoveel keer gooien als degene van de highscore
+        await ctx.send('Typ $gooi om te gooien.')
+        await bot.wait_for("message", check=checkGooi)
+        await asyncio.sleep(1)
+        await ctx.send('Aantal worpen: ' + str(i))
+
+        score = worp.score #haalt score op bij gooi fuc 
+        player.setScore(Score(player.name, player.discriminator, score, i)) #slaat score op in player
+        print("Players score: " + str(player.score)) #debug option
+
+        if score == 7: #bij 7 mag je niet opnieuw
+            break
+
+        if i < game.highScore.tries: #zonder dit zou je bij de laatste beurt deze vraag ook nog krijgen
+          await ctx.send('\n\nWil je opnieuw gooien? (J/N)')
+          msg = await bot.wait_for("message", check=checkAnswer) #captures the msg
+          msgContent = msg.content.lower()
+          #await ctx.send(msgContent) #debug option
+          
+          if msgContent[0] == "n": #breaks out of for loop (end turn)
+            break
+
+      player.active = False #ends turn
+      if player == game.participants[0] or player.score > game.highScore.score: #highscore set at 1st player or when score is improved
+        game.setHighScore(player)
+      
+
+    #gelijkspel phase
+    game.checkDraw() #checkt voor gelijkspel na de ronde
+    if game.drawPlayers:
+      await ctx.send("######################################" )
+      await ctx.send(f"Er is een gelijkspel! Deze spelers gooiden allemaal {game.highScore.printScore().lower()}:")
+      for player in game.drawPlayers: await ctx.send(player.name)
+      await ctx.send("Deze spelers mogen nu allemaal 1 dobbelsteen gooien, de hoogste wint!" )
+      await ctx.send("######################################" )
+
+    while len(game.drawPlayers) > 1:
+      for player in game.drawPlayers:
+        player.active = True
+        mention = '<@' + str(player.discriminator) + '>'
+        await ctx.send(mention + ' is aan de beurt! Typ $gooiEen om een teerling te werpen.')
+
+        #waits for throwone
+        await bot.wait_for("message", check=checkGooiEen)
+        drawThrow = Dice().roll() 
+        image = discord.File('img/' + str(drawThrow) + '.png')
+        await ctx.send(file=image)
+        player.drawScore = drawThrow #saves the drawScore in the player (in drawPlayers)
+        player.active = False
+
+      game.drawPlayers.sort(key=takeDrawScore, reverse=True) #puts the highest throw first
+      game.removeDrawPlayers() #removes every lower score
+      
+    game.checkWinner()
+
+    #einde van de ronde; speler met hoogste begint, laagste zuipt?
+    await ctx.send("Einde van deze ronde, het spel gaat automatisch verder. Typ '$stop' om te stoppen met spelen." )
+    await ctx.send("######################################" )
+    await ctx.send(f"De winnaar van deze ronde is {game.participants[0].name}, met een worp van {game.highScore.printScore().lower()}!")
+    await ctx.send("######################################" )
+    await ctx.send(f"En de verliezer van deze ronde is {game.participants[-1].name}, 3 slokken voor u!" )
+    await ctx.send("######################################" )
+
+@bot.command()
+async def stop(ctx):
+  game.stop()
+  await ctx.send("De game wordt gestopt na deze ronde. Yuuw!" )
+
+bot.run(os.getenv('TOKEN'))
